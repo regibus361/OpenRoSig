8ff75d416cc7a9e62849a321e37a726d56e2d3a8
regibus361 <70603887+regibus361@users.noreply.github.com>
2021-07-13 12:30:08 +0100

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -8,7 +8,8 @@ return {
 			SignalLower = 'Lower',
 			SignalUpper = 'Upper',
 			TrainHitter = 'SignalTriggerPart', -- At least one at the front and one at the back.
-			SignalDetector = 'SignalDetector'
+			SignalDetector = 'SignalDetector',
+			AWSDetector = 'AWSMagnet'
 		}
 	},
 	
@@ -30,6 +31,32 @@ return {
 		{ Color3.fromRGB(255, 255, 0), Color3.fromRGB(255, 255, 0) },
 		{ Color3.fromRGB(0, 0, 0), Color3.fromRGB(0, 255, 0) }
 	},
+	
+	AWS = {
+		DetectionDebounce = 15, -- More detections on one detector within this time period will not fire
+		
+		KeypressEvent = game.ReplicatedStorage.OpenRoSigKeypressEvent, -- Fire this on the client to deactivate!
+		NotClearIsLooped = true, -- Whether to continue playing the 'not clear' sound until a key is pressed
+		NotClearTimeout = 6, -- After this, the AWSTimedOut function is called. nil for no timeout
+		
+		ClearSoundID = 273295455, -- Example sounds by hhwheat, from roblox.com/library/1556802253
+		NotClearSoundID = 273295334,
+		SoundProperties = { Volume = 10, RollOffMaxDistance = 25 }, -- Additional properties to apply.
+		
+		GetPlayer = function(Hitter) -- Player driving the train. Can return nil
+			return Hitter.Parent.DCCProperties.CurrentPlayer.Value
+		end,
+		
+		GetSoundEminator = function(Hitter)
+			return Hitter.Parent.Base
+		end,
+		
+		TimedOut = function(Hitter)
+			warn('You took too long!')
+		end,
+	},
+	
+	
 	
 	NewLight = function(Parent)
 		-- Returns a light parented to the parent
